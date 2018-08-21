62ade987319266979b2140454e36fce34af93e17
etaba <eptaba@gmail.com>
2018-08-21 08:09:51 -0700

    group always open

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 setup(
   name = 'tickerbar',
   packages = ['tickerbar'],
-  version = '0.6',
+  version = '0.7',
   description = 'Stock quotes on the mac touch bar',
   author = 'Eric Taba',
   author_email = 'eptaba@gmail.com',
